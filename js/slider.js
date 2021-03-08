66c57cdf34cddc46f92f2a0e4d17249a86987012
Vineeth pawar R <vineethpawar99@gmail.com>
2021-03-08 14:21:31 +0530

    abc

---
+++ js/slider.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper('.swiper-container', {
+	effect: 'zoom',
+slidesPerView: 3,
+spaceBetween: 30,
+slidesPerGroup: 3,
+loop: true,
+loopFillGroupWithBlank: true,
+pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+},
+navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+},
+breakpoints: {
+  0:{
+	slidesPerView: 1,
+	spaceBetween: 10,
+  },
+  640: {
+	slidesPerView: 2,
+	spaceBetween: 20,
+  },
+  768: {
+	slidesPerView: 3,
+	spaceBetween: 40,
+  },
+  1024: {
+	slidesPerView: 3,
+	spaceBetween: 0,
+  },
+},
+
+});
